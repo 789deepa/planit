79f1d02860727a8d9bec79213c1f4e312f8971cb
789deepa <dspatil371123@gmail.com>
2026-06-26 17:58:11 +0530

    Added mock data

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import GiftForm from "./components/GiftForm";
+import mockData from "./data/mockData.js";
 
 function App() {
+
+  console.log(mockData);
   return (
-    <div className="min-h-screen bg-rose-50 flex items-center justify-center">
-      <GiftForm />
-      <h1 className="text-3xl font-bold text-rose-500">PlanIt 🎁</h1>
+    <div className="min-h-screen bg-rose-50 flex flex-col items-center justify-center">
+
+      <h1 className="text-5xl font-extrabold text-rose-500 mb-8">
+        PlanIt 🎁
+      </h1>
+
+      <p className="text-gray-700 mb-5 text-center">
+        AI-powered gift Planner.
+      </p>
+
+      <div className="bg-white rounded-3xl shadow-xl p-8 w-full max-w-xl">
+       <GiftForm />
+      </div>
+      
     </div>
   )
 }
