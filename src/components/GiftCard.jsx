0124436dca5e3b2b3f9f3a9d9d1c881074477111
789deepa <dspatil371123@gmail.com>
2026-06-26 20:13:12 +0530

    maintain

--- src/components/GiftCard.jsx
+++ src/components/GiftCard.jsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
+
 function GiftCard ( { gift }) {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+
     return (
         <div className="bg-white rounded-2xl shadow-md p-5"> 
             <h3 className="text-xl font-bold text-gray-800">
                 {gift.name}
             </h3>
 
+        <button
+        onClick={() => setIsOpen(!isOpen)}
+        className="mt-4 text-rose-500 font-medium">
+            {isOpen ? "Hide Instructions" : "Show Instructions"}
+        </button>
+
+        {isOpen && (
             <p className="text-gray-600 mt-3">
                 {gift.instructions}
             </p>
+        )}
+        
         </div>
     );
 }
